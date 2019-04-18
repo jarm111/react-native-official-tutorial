5369c56108b00a41b4b196810b12fe656d940867
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-04-18 10:45:19 +0300

    Add Bananas

--- screens/HelloWorldScreen.js
+++ screens/HelloWorldScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 import HelloWorld from '../components/HelloWorld';
+import Bananas from '../components/Bananas';
 
 const HelloWorldScreen = () => {
-  return <HelloWorld />;
+  return (
+    <View>
+      <HelloWorld />
+      <Bananas />
+    </View>
+  );
 };
 
 export default HelloWorldScreen;
