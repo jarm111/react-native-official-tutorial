59df05cd78cd9035b3aa8152d034160d3faaa9bb
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-04-22 11:00:02 +0300

    Create Touchables

--- screens/MiscScreen.js
+++ screens/MiscScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PizzaTranslator from '../components/PizzaTranslator';
 import ButtonBasics from '../components/ButtonBasics';
+import Touchables from '../components/Touchables';
 
 const MiscScreen = () => {
-  const components = [<PizzaTranslator />, <ButtonBasics />];
+  const components = [<PizzaTranslator />, <ButtonBasics />, <Touchables />];
 
-  return components[1];
+  return components[2];
 };
 
 export default MiscScreen;
